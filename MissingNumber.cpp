eb6c6e5b540be9cb4730fecb9bef0c6d1e4b2e4f
HARSH DAYAL <harshdayal149@gmail.com>
2023-03-16 11:47:27 +0530

    Create MissingNumber.cpp

---
+++ MissingNumber.cpp
@@ -0,0 +1,9 @@
+int missingNumber(vector<int>&a, int N) {
+    int xor1 = 0, xor2 = 0;
+    int n = N-1;
+    for(int i = 0; i < n; i++){
+        xor2 = xor2 ^ a[i];
+        xor1 = xor1 ^ (i+1);
+    }
+    xor1 = xor1 ^ N;
+    return xor1 ^ xor2;
