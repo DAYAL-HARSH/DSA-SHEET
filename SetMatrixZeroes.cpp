805eb9e3e9af1131b93557176a720e367c05ac7a
HARSH DAYAL <harshdayal149@gmail.com>
2023-03-26 14:40:21 +0530

    Update SetMatrixZeroes.cpp

--- SetMatrixZeroes.cpp
+++ SetMatrixZeroes.cpp
@@ -23,3 +23,40 @@ public:
         }
     }
 };
+
+
+// GFG question, this ques is a bit different from above instead of making both whole row and column we are doing  
+/// in whole matrix when you find a zero, convert its upper, lower, left, and right value to zero and make that element 
+// the sum of the upper, lower, left and right value.
+
+class Solution {
+public:
+    void MakeZeros(vector<vector<int> >& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<vector<int>> temp = matrix;
+        
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(temp[i][j] == 0){
+                    if(i+1 < rows){
+                        matrix[i][j] += temp[i+1][j];
+                        matrix[i+1][j] = 0;
+                    }
+                    if(i-1 >= 0){
+                        matrix[i][j] += temp[i-1][j];
+                        matrix[i-1][j] = 0;
+                    }
+                    if(j+1 < cols){
+                        matrix[i][j] += temp[i][j+1];
+                        matrix[i][j+1] = 0;
+                    }
+                    if(j-1 >= 0){
+                        matrix[i][j] += temp[i][j-1];
+                        matrix[i][j-1] = 0;
+                    }
+                }
+            }
+        }
+    }
+};
