bed6fc02d41fd520644b99262fc2b8012ea25a5f
HARSH DAYAL <harshdayal149@gmail.com>
2023-04-07 17:11:08 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -4,15 +4,13 @@
 using namespace std;
 
 
-void bubble_sort(int arr[], int n){
-    for(int i = 0; i <= n-2; i++){
-        int minimum = i;
-        for(int j = i; j <= n-1; j++){
-            if(arr[j] < arr[minimum]) minimum = j;
+void selection_sort(int arr[], int n){
+    for(int i = n-1; i >= 1; i--){
+        for(int j = 0; j <= i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
         }
-        int temp = arr[minimum];
-        arr[minimum] = arr[i];
-        arr[i] = temp;
     }
     for(int i = 0; i < n; i++){
         cout<<arr[i]<<" ";
