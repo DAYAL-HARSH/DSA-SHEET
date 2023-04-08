e4fc58d5f992b4bb50ea224ad89d191af9b32f11
HARSH DAYAL <harshdayal149@gmail.com>
2023-04-08 11:50:35 +0530

    Create QuickSort.cpp

---
+++ QuickSort.cpp
@@ -0,0 +1,8 @@
+/// Intution
+// pick a pivot and place it in its correct place in the array
+// pivot can be->
+// 1. first element in the array
+// 2. last element in the array
+// 3. median of the array
+// random element of the array
+
