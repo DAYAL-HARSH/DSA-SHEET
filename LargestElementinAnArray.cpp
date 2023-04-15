5e0f14f4afbe86867ce6df32e32dfb75e70287ca
HARSH DAYAL <harshdayal149@gmail.com>
2023-04-15 15:13:20 +0530

    Create LargestElementinAnArray.cpp

---
+++ LargestElementinAnArray.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    int largest = arr[0];
+    for(int i = 0; i < n; i++){
+        if(arr[i] > largest){
+            largest = arr[i];
+        }
+    }
+    cout<<largest;
+    return 0;
+}
