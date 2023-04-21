4e17b153d9ce90a7763cf5c0a734b89e4e69a9e3
HARSH DAYAL <harshdayal149@gmail.com>
2023-04-21 11:19:53 +0530

    Update 4Sum.cpp

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -1,3 +1,30 @@
+// Better approach
+code -> 
+  #include<bits/stdc++.h>
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    // Write your code here
+    int n = nums.size();
+    set<vector<int>> st;
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            set<long long> hashset;
+            for(int k = j + 1; k < n; k++){
+                long long sum = nums[i] + nums[j];
+                sum += nums[k];
+                long long forth = target - (sum);
+                if(hashset.find(forth) != hashset.end()){
+                  vector<int> temp = {nums[i], nums[j], nums[k], (int)forth};
+                  sort(temp.begin(), temp.end());
+                  st.insert(temp);
+                }
+                hashset.insert(nums[k]);
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+
 // optimal approach 
 code -> 
   class Solution {
@@ -48,3 +75,38 @@ public:
         return res;
     }
 };
+
+
+codestudio code -> 
+#include<bits/stdc++.h>
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    // Write your code here
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> ans;
+    int n = nums.size();
+    for(int i = 0; i < n; i++){
+        if(i > 0 && nums[i] == nums[i-1]) continue;
+        for(int j = i + 1; j < n; j++){
+            if(j != (i+1) && nums[j] == nums[j-1]) continue;
+            int k = j + 1;
+            int l = n - 1;
+            while(k < l){
+                long long sum = nums[i];
+                sum += nums[j];
+                sum += nums[k];
+                sum += nums[l];
+                if(sum == target){
+                  vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                  ans.push_back(temp);
+                  k++;
+                  l--;
+                  while(k < l && nums[k] == nums[k-1]) k++;
+                  while(k < l && nums[l] == nums[l+1]) l--;
+                } 
+                else if (sum < target) k++;
+                else l--;
+            }
+        }
+    }
+    return ans;
+}
