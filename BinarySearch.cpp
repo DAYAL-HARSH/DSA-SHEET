0b8cf4a68779de0e6e033917b132f833f6710a7b
HARSH DAYAL <harshdayal149@gmail.com>
2023-06-13 10:03:55 +0530

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,3 +1,5 @@
+1. Iterative Binary Search pseudo code->
+code->
 int solve(int n, vector<int> nums, int target){
     int low = 0;
     int high = n-1;
@@ -14,4 +16,28 @@ int solve(int n, vector<int> nums, int target){
         }
     }
     return -1;
+}
+
+
+2. Recursive Binary Search Pseudo code->
+code->
+int Binary_search(vector<int>& nums, int low, int high, int target)
+{
+    if(low > high)
+    {
+        return -1;
+    }
+    int mid = (low+high)/2;
+    if(nums[mid] == target){
+        return mid;
+    }
+    else if(target > nums[mid]){
+        return Binary_search(nums, mid+1, high, target);
+    }
+    return Binary_search(nums, low, mid-1, target);
+}
+
+int search(vector<int>& nums, int target) {
+    // Write Your Code Here
+    return Binary_search(nums, 0, nums.size()-1, target);
 }
