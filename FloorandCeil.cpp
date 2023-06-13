62c1fd4cc859f041680850edf830b483cd153ea8
HARSH DAYAL <harshdayal149@gmail.com>
2023-06-13 12:47:54 +0530

    Create FloorandCeil.cpp

---
+++ FloorandCeil.cpp
@@ -0,0 +1,44 @@
+int findFloor(int arr[], int n, int x) {
+	int low = 0, high = n - 1;
+	int ans = -1;
+
+	while (low <= high) {
+		int mid = (low + high) / 2;
+		// maybe an answer
+		if (arr[mid] <= x) {
+			ans = arr[mid];
+			//look for smaller index on the left
+			low = mid + 1;
+		}
+		else {
+			high = mid - 1; // look on the right
+		}
+	}
+	return ans;
+}
+
+int findCeil(int arr[], int n, int x) {
+	int low = 0, high = n - 1;
+	int ans = -1;
+
+	while (low <= high) {
+		int mid = (low + high) / 2;
+		// maybe an answer
+		if (arr[mid] >= x) {
+			ans = arr[mid];
+			//look for smaller index on the left
+			high = mid - 1;
+		}
+		else {
+			low = mid + 1; // look on the right
+		}
+	}
+	return ans;
+}
+
+pair<int, int> getFloorAndCeil(int arr[], int n, int x) {
+	sort(arr, arr+n);
+	int f = findFloor(arr, n, x);
+	int c = findCeil(arr, n, x);
+	return {f,c};
+}
